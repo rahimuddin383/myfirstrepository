693334db3d3fb1cfbfe5221742d7a99c2ef432b3
khan_rahimuddin <khan_rahimuddin@yahoo.in>
2021-12-04 13:35:05 +0530

    Added comments in StockController.cs file

--- Controllers/StockController.cs
+++ Controllers/StockController.cs
@@ -7,7 +7,9 @@ using Microsoft.Extensions.Logging;
 
 namespace VS2019_Demos.Controllers
 {
-public class StockController : ControllerBase{
-
-}
+    public class StockController : ControllerBase{
+        //Hello friends how are you, I am from India
+        //Hit me once you reach home
+        //I will be waiting
+    }
 }
